fb8a3f39ece9e0d8cf6a6f411ffd3784a4531b0a
Matthew Ceroni <matthew.ceroni@smarsh.com>
2017-06-19 15:31:29 -0700

    Moved all outputs to output.tf.

---
+++ output.tf
@@ -0,0 +1,30 @@
+/* VPC resource outputs */
+output "vpc_id" {
+  value = "${aws_vpc.environment.id}"
+}
+
+output "vpc_main_rt_id" {
+  value = "${aws_vpc.environment.main_rt_id}"
+}
+
+output "vpc_cidr_block" {
+  value = "${aws_vpc.environment.cidr_block}"
+}
+
+/* Subnet resource outputs */
+
+output "prvt_subnet_id" {
+  value = [ "${aws_subnet.private.*.id}" ]
+}
+
+output "prvt_subnet_cidr" {
+  value = [ "${aws_subnet.private.*.cidr_block}" ]
+}
+
+output "public_subnet_id" {
+  value = [ "${aws_subnet.public.*.id}" ]
+}
+
+output "public_subnet_cidr" {
+  value = [ "${aws_subnet.public.*.cidr_block}" ]
+}
