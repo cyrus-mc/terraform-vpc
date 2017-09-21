34ccd37b117562fde4b75157f0bdf6960b789ce8
Matthew Ceroni <matthew.ceroni@smarsh.com>
2017-09-21 11:43:18 -0700

    Set defaults for various variables.

--- variables.tf
+++ variables.tf
@@ -9,16 +9,26 @@ variable "cidr_block" {
   description = "VPC CIDR block"
 }
 
-variable "cidr_block_bits" {}
+variable "cidr_block_bits" {
+  default = "8"
+}
 
-variable "cidr_block_start" {}
-variable "cidr_block_end" {}
+variable "cidr_block_start" {
+  default = "254"
+}
+
+variable "cidr_block_end" {
+  default = "255"
+}
 
 variable "environment" {}
 
 variable "customer_gateway_id" {}
 
-variable "key_name" {}
+variable "key_name" {
+  description = "Only needed for GovCloud, key to use for NAT instance"
+  default     = ""
+}
 
 variable "aws_govcloud" {
   description = "Deployment into AWS GovCloud region"
