0ff2583179bd8f1bb63750424fe8be84717e9509
Matthew Ceroni <matthew.ceroni@dat.com>
2019-06-14 15:05:28 -0700

    Added outputs of subnet ARN

--- output.tf
+++ output.tf
@@ -5,6 +5,8 @@ output "vpc_cidr_block" { value = aws_vpc.environment.cidr_block }
 
 /* Subnet resource outputs */
 output "prvt_subnet_id"     { value = aws_subnet.private.*.id }
+output "prvt_subnet_arn"    { value = aws_subnet.private.*.arn }
 output "prvt_subnet_cidr"   { value = aws_subnet.private.*.cidr_block }
 output "public_subnet_id"   { value = aws_subnet.public.*.id }
+output "public_subnet_arn"  { value = aws_subnet.public.*.arn }
 output "public_subnet_cidr" { value = aws_subnet.public.*.cidr_block }
