2510691f01418d2e6ae50c326ef42e59ed101a17
Matt Ceroni <matthew.ceroni@dat.com>
2020-07-12 00:29:40 +0000

    Merged in route_table_id_output (pull request #10)
    Output private and public route table IDs.
    
    Approved-by: Matt Ceroni <matthew.ceroni@dat.com>

--- output.tf
+++ output.tf
@@ -3,10 +3,14 @@ output "vpc_id"         { value = aws_vpc.environment.id }
 output "vpc_main_rt_id" { value = aws_vpc.environment.main_route_table_id }
 output "vpc_cidr_block" { value = aws_vpc.environment.cidr_block }
 
-/* Subnet resource outputs */
+/* subnet resource outputs */
 output "prvt_subnet_id"     { value = aws_subnet.private.*.id }
 output "prvt_subnet_arn"    { value = aws_subnet.private.*.arn }
 output "prvt_subnet_cidr"   { value = aws_subnet.private.*.cidr_block }
 output "public_subnet_id"   { value = aws_subnet.public.*.id }
 output "public_subnet_arn"  { value = aws_subnet.public.*.arn }
 output "public_subnet_cidr" { value = aws_subnet.public.*.cidr_block }
+
+/* route table outputs */
+output "prvt_route_table_id"   { value = aws_route_table.private.*.id }
+output "public_route_table_id" { value = aws_route_table.public.*.id }
